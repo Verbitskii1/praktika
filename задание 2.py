8b5aab507ab3351ebd70fbaa72a2c98914b3e378
Anvir07 <161944248+Anvir07@users.noreply.github.com>
2025-09-18 19:33:36 +0300

    Add files via upload

---
+++ задание 2.py
@@ -0,0 +1,16 @@
+# номер 3
+from math import *
+x = 3.74 * 10 ** -2
+y = -0.825
+z = 0.16*10**2
+s = (1 + sin(x + y)**2) / (abs(x - 2 * y/1 + x ** 2 * y ** 2)) * x**abs(y) + cos(atan(1/z))**2
+print(s)
+
+
+# номер 4
+from math import *
+x = 0.4 * 10**4
+y = -0.875
+z =  - 0.475 * 10**-3
+s = abs(cos(x) - cos(y)) ** (1 + 2 * sin(y)**2) * (1 + z + z**2/2 + z**3/3 + z ** 4/4)
+print(s)
