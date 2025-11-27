e4a570c592d1ad42893de8844026ae67e7f19e01
Андрей Вербицкий <161944248+Verbitskii1@users.noreply.github.com>
2025-11-27 22:28:53 +0300

    Add files via upload

---
+++ задание 10.py
@@ -0,0 +1,13 @@
+# A = [[1, 2, 3],[2, 5, 6],[3, 6, 4]]
+# with open('ВербицкийАндрейСтаниславоич_у252_vvod.txt', 'r') as f:
+#     l = f.readline()
+# A = eval(l)
+# flag = True
+# for i in range(len(A)):
+#     for j in range(i + 1, len(A)):
+#         if A[i][j] != A[j][i]:
+#             flag = False
+#             break
+# print(flag)
+# with open('ВербицкийАндрейСтаниславоич_у252_vivod.txt', 'w') as f:
+#     f.write(f'{flag}\n')
