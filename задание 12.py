674e91d6b36ab97f456320dd80893f509435cba6
Андрей Вербицкий <161944248+Verbitskii1@users.noreply.github.com>
2025-12-09 21:53:44 +0300

    Add files via upload

---
+++ задание 12.py
@@ -0,0 +1,18 @@
+# задание из блока А
+# задание 2
+# def n (a,b):
+#     if a < b:
+#         return a
+#     return n(a-b,b)
+# print(n(15,8))
+
+# задание из блока B
+# задание 1
+# def mx():
+#     n = int(input())
+#     if n == 0:
+#         return float('-inf')
+#     return max(n, mx())
+# res = mx()
+# print("Наибольшее число:", res)
+
